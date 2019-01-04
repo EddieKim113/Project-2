ea7e8266d0318965184454d9e90670cb4946a51d
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2019-01-03 19:23:47 -0800

    renamed index.html to home.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
     });
 
     app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/menu", function(req,res) {
