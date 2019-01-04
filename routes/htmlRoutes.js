6d87d76e9cfa3ab39b70754fb6ebf2ef76bebf64
ganjioppa <Timkim@Tims-MacBook.local>
2019-01-03 20:16:47 -0800

    .

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,11 +10,11 @@ module.exports = function(app) {
 
     app.get("/", function(req, res) {
         console.log("landing page");
-        res.sendFile(path.join(__dirname, '../public/landing.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
     app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
     app.get("/menu", function(req,res) {
