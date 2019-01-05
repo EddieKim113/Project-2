456b32322ac39f55b5df3d0d3d1ffc12e38977d5
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2019-01-04 20:32:25 -0800

    post route

--- controllers/reservation_controller.js
+++ controllers/reservation_controller.js
@@ -17,6 +17,16 @@ exports.index = function(req, res) {
 
 };
 
-// exports.makeReservation = function(req, res) {
-
-// }
+exports.makeReservation = function(req, res) {
+    db.Reservation.create({
+        name: req.body.name,
+        phone: req.body.phone,
+        type: req.body.type,
+        email: req.body.email,
+        partySize: req.body.partySize,
+        date: req.body.date,
+        time: req.body.time
+    }).then(function(dbReservation) {
+        res.json(dbReservation);
+    });
+}
